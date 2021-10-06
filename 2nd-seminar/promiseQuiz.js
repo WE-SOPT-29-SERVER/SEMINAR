4b1fb6e700081fa8b2a2cab36e70be85a4411a9b
Woo-yeong Kim <kimwy1997@gmail.com>
2021-10-07 04:25:15 +0900

    Create files for 2nd-seminar

---
+++ 2nd-seminar/promiseQuiz.js
@@ -0,0 +1,42 @@
+const members = require("./members");
+
+const getOnline = members => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const data = members.filter(o => o.location === "online");
+      resolve(data);
+    }, 500);
+  });
+};
+
+const getOffline = members => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const data = members.filter(o => o.location === "offline");
+      resolve(data);
+    }, 500);
+  });
+};
+
+const getYB = members => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const data = members.filter(o => o.group === "YB");
+      resolve(data);
+    }, 500);
+  });
+};
+
+const getOB = members => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const data = members.filter(o => o.group === "OB");
+      resolve(data);
+    }, 500);
+  });
+};
+
+getOnline().then().then();
+
+// // getOnline(members).then(getOB).then(console.log);
+// getYB(members).then(getOffline).then(console.log);
